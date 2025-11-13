ccd06561c935250ec869167a63e4cfab90a2e3c1
David Benei <beneidavid.it@gmail.com>
2025-11-14 00:03:11 +0100

    Add Monitor initialization to main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from controller import Controller
+from monitor import Monitor
 from wireframe import start_gui
 import paho.mqtt.client as paho
 
 
 def main():
-    controller = Controller(client_id='Controller', protocol=paho.MQTTv5)
-    controller.connect(host="910e146c7f1f4c0fa6799235de0cd0fe.s1.eu.hivemq.cloud", port=8883,
-                       username="main_connection", password="dycrax-3ruzdU")
+    HOST = "910e146c7f1f4c0fa6799235de0cd0fe.s1.eu.hivemq.cloud"
+    PORT = 8883
+    USERNAME = "main_connection"
+    PASSWORD = "dycrax-3ruzdU"
 
+    controller = Controller(client_id='Controller', protocol=paho.MQTTv5)
+    controller.connect(HOST, PORT, USERNAME, PASSWORD)
     controller.start()
+    print("CONTROLLER: Service Started.")
+
+    monitor = Monitor(client_id='Monitor_Service', protocol=paho.MQTTv5)
+    monitor.connect(HOST, PORT, USERNAME, PASSWORD)
+    monitor.start()
+    print("MONITOR: Service Started.")
 
 
 if __name__ == "__main__":
